798b406cad4cc027ca8c49ed3d289507cd558a5d
DePlaced <117630280+DePlaced@users.noreply.github.com>
2023-03-07 11:09:26 +0100

    ctrl GROUP

---
+++ src/controller/GroupCtrl.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.util.List;
+
+import db.DataAccessException;
+import model.Group;
+
+public class GroupCtrl implements GroupCtrIF {
+
+	@Override
+	public List<Group> findAll() throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Group findById(int id) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
